9e926befe61f39f26609c9b187a4a4bc26efe38d
BK-Bagchi <bkbagchi.dipto@gmail.com>
2022-09-13 12:05:39 +0600

    models created and data added to db

--- django_portfolio/models.py
+++ django_portfolio/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class SkillDetail(models.Model):
+    skill = models.CharField(max_length=50)
+    percentage = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.skill+" "+self.percentage
+
+
+class AdditionalSkills(models.Model):
+    skill = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.skill
